d2818d4c9ff55c6160033ec73e0768e5bdf245b7
Soykot Hosen <soykothosen1603028@gmail.com>
2022-01-04 01:18:09 +0600

    Project Complete

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mysql = require("mysql");
 
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.set('view engine','ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+
+var db = mysql.createConnection({
+    host    : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'petshop'
+  });
+  
+  db.connect(function(err){
+      if(err){
+          throw err;
+      }
+      console.log("Database is connected");
+  });
 
 
 app.get('/', function(req,res){
 
 
-    res.render('Home',{});
+    res.render('Home.ejs',{loged: 0});
 
 });
 
@@ -42,7 +58,39 @@ app.post('/registration', function(req,res){
     console.log(password);
 
 
-    res.render('login.ejs',{});
+
+
+    db.query("SELECT * FROM `user_information` WHERE username = ? ;",[username],function(error,results){
+
+        if(error){
+            throw error
+        } else{
+            console.log(results.length);
+
+            if(results.length == 0){
+
+                db.query("INSERT INTO `user_information`(`username`, `email`, `password`) VALUES (?,?,?)",[username,email,password],function(err, result){
+
+                    if(err){
+                        throw err;
+                    } else{
+                        res.render('Home.ejs',{loged: 1, username: username});
+                    }
+
+
+
+                });
+
+            } else{
+                res.send("Username already Exist");
+            }
+
+        }
+
+    });
+
+
+    //res.render('login.ejs',{});
 
 
 });
@@ -58,8 +106,32 @@ app.post('/login', function(req,res){
     console.log(email);
     console.log(password);
 
+    db.query("SELECT * FROM `user_information` WHERE username = ? ;",[username],function(err, results, fields){
+        if(err){
+          throw err;
+        } else{
+            console.log(results);
 
-   res.send("Log In Successful");
+            if(email == results[0].email){
+
+                if(password == results[0].password){
+                    res.render('Home.ejs',{loged: 1, username: results[0].username});
+                } else{
+                    res.send("Wrong Password");
+                }
+
+            } else{
+                res.send("Wrong Email Address");
+            }
+
+        }
+
+    });
+
+        
+
+
+   //res.send("Log In Successful");
 
 
 });
